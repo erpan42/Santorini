27c72e2f5ea126cf769101b5d3c40f5a3c13cfa3
erpan42 <yierpan42@gmail.com>
2024-04-09 00:50:08 -0400

    frontend setup complete using react

---
+++ Santorini/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; // Import the CSS file
+import Game from './Game';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Game />
+    </div>
+  );
+};
+
+export default App;
